1eaf1e0f6bb7ff12c2914211aaca07274bac1287
Karl Heinz Marbaise <kama@soebes.de>
2023-03-27 21:42:09 +0200

    Keep records.

--- src/main/java/com/soebes/ip/IPv4Address.java
+++ src/main/java/com/soebes/ip/IPv4Address.java
@@ -1,6 +1,7 @@
 package com.soebes.ip;
 
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.function.Function;
 
 public record IPv4Address(int t1, int t2, int t3, int t4) {
@@ -8,6 +9,18 @@ public record IPv4Address(int t1, int t2, int t3, int t4) {
   public String toString() {
     return t1 + "." + t2 + "." + t3 + "." + t4;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (IPv4Address) obj;
+    return this.t1 == that.t1 &&
+           this.t2 == that.t2 &&
+           this.t3 == that.t3 &&
+           this.t4 == that.t4;
+  }
+
 
   static Comparator<IPv4Address> IP_ADDRESS_COMPARATOR = Comparator
       .comparingInt(IPv4Address::t1)
